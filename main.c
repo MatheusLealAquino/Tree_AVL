bedaeee7205d20a91910707d638be1c9f2a72f6a
Matheus Leal <matheusaquino@id.uff.br>
2017-06-22 13:38:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "avl.h"
-
+                //Done by: Matheus Leal
+                //June at 2017
 int main(){
     No * p = criaAvl(p,5);
     p = insereAvl(p,6);
